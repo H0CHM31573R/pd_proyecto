bf55939c5116e009ea00bf7dc8985cce576744e2
H0CHM31573R <yancosr@rocketmail.com>
2022-11-26 15:21:16 -0600

    Se agrega sitio web estatico

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from enum import Enum
 
 #fastapi
 from fastapi import FastAPI
-#from fastapi import Body, Query, Path, File, UploadFile
 from fastapi import File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse 
 
 from numpy import fromstring, uint8, reshape
 from cv2 import imdecode, cvtColor, resize, IMREAD_COLOR, COLOR_BGR2RGB, INTER_AREA
@@ -17,6 +18,7 @@ from cv2 import imdecode, cvtColor, resize, IMREAD_COLOR, COLOR_BGR2RGB, INTER_A
 from tensorflow.keras.models import load_model
 
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='static',html=True))
 
 origins = ["*"]
 
@@ -30,10 +32,14 @@ app.add_middleware(
 
 sign_model = load_model("simple_cnn.h5")
 
+classes = ["A", "B", "C", "D", "del", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "nothing",
+           "O", "P", "Q", "R", "S", "space", "T", "U", "V", "W", "X", "Y", "Z"]
+
 @app.get("/", tags=["Root"])
 async def read_root():
-    return {"Source": "You can find my source code at: https://github.com/H0CHM31573R/pd_proyecto", 
-            "API": "You can test my API at: http://ec2-3-82-149-136.compute-1.amazonaws.com/docs"}
+    # return {"Source": "You can find my source code at: https://github.com/H0CHM31573R/pd_proyecto", 
+            # "API": "You can test my API at: http://ec2-3-82-149-136.compute-1.amazonaws.com/docs"}
+    return FileResponse('index.html')
     
 @app.post("/model/sign")
 async def analizar_imagen(image:UploadFile = File(...)):
@@ -46,7 +52,8 @@ async def analizar_imagen(image:UploadFile = File(...)):
     img = img/255.0
     inp = img.reshape(1, 150, 150, 3)
     pred = sign_model.predict(inp)
+    pred_class = pred.argmax(axis=-1)
     
     return {
-        "prediction": pred.tolist(),
+        "prediction": classes[pred_class],
     }
